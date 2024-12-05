33dd074afb401b3074006e6a93cab640a44735d5
daffaalex22 <daffaalex22@gmail.com>
2024-12-05 14:26:21 +0700

    adblocker added

--- content.js
+++ content.js
@@ -1,8 +1,33 @@
-window.addEventListener('message', (event) => {
-  if (event.source !== window) return;
-  if (event.data.type && event.data.type === 'ADS_POST') {
-    console.log('AdsPost', event.data.adsPosts);
+const isAdPost = (post) => {
+  if (!post) return false
 
-    // TODO: Hide the ad posts
+  const span = post.querySelectorAll('article div[dir="ltr"] > span')[3];
+  if (span?.textContent === "Ad") return true
+
+  return false
+}
+
+const adsRemover = (mutationsList, observer) => {
+  for (let mutation of mutationsList) {
+    // If the mutation isn't a new post
+    if (
+      mutation.type !== 'childList' ||
+      mutation.addedNodes.length <= 0
+    ) continue
+
+    mutation.addedNodes.forEach(node => {
+      // If it's an ad post
+      if (
+        node.nodeType === Node.ELEMENT_NODE && 
+        node.getAttribute("data-testid") == "cellInnerDiv" &&
+        isAdPost(node)
+      ) {
+        console.log("[Xtra] An ad is removed")
+        node.style.display = "none";
+      }
+    });
   }
-});
+};
+
+const observer = new MutationObserver(adsRemover);
+observer.observe(document.body, { childList: true, subtree: true });
